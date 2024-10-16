f7ff06c0dda4390937ce4e92053da80723974076
Samucada <samuelsandro11@gmail.com>
2024-10-16 20:57:47 -0300

    .

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -23,7 +23,7 @@ function Home() {
                             href={`https://wa.me/${5515988150433}`}
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="mt-5 inline-flex items-center px-5 py-2 bg-[var(--cor-amarelo)] text-white rounded-lg transition-colors duration-300 hover:bg-yellow-400 font-medium"
+                            className="mt-5 inline-flex items-center px-5 py-2 bg-[var(--cor-amarelo)] text-white rounded-lg transition-colors duration-300 hover:bg-[#003366] font-medium"
                         >
                             Contato <FaWhatsapp className="ml-2 h-8" />
                         </a>
@@ -44,28 +44,29 @@ function Home() {
                 <div className="absolute inset-0 bg-blue-900 opacity-70"></div>
 
                 <section className="flex relative container mx-auto w-full h-screen items-center justify-between z-10">
-                    <div className="flex flex-col justify-start items-start p-10 max-w-2xl w-full gap-3 backdrop-blur-md rounded-lg shadow-lg bg-white z-20">
-                        <h1 className="text-6xl font-bold text-left text-[#003366]">
+                    <div className="flex flex-col justify-start items-start p-10 max-w-2xl w-full gap-3 backdrop-blur-md rounded-lg shadow-lg bg-white z-20 ml-auto">
+                        <h1 className="text-6xl font-bold text-left text-[#FFD700]">
                             Crie com Nossa Equipe
                         </h1>
                         <p className="text-2xl mt-2 text-left text-[#333333]">
-                            Nossa equipe dedicada está pronta para transformar seus sonhos em realidade.
+                            Em Sorocaba, SP, transformamos a visão em realidade. Oferecemos serviços superiores de engenharia elétrica, civil e arquitetura, combinando inovação com excelência. Quer se trate de projetos residenciais ou comerciais, nossa expertise garante soluções eficientes e estéticas que superam expectativas. Confie em quem entende de construção com precisão e paixão. Vamos construir juntos.
+
                         </p>
                         <a
                             href={`https://wa.me/${5515988150433}`}
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="mt-5 inline-flex items-center px-5 py-2 bg-[var(--cor-amarelo)] text-white rounded-lg transition-colors duration-300 hover:bg-yellow-400 font-medium"
+                            className="mt-5 inline-flex items-center px-5 py-2 bg-[var(--cor-azul)] text-white rounded-lg transition-colors duration-300 hover:bg-yellow-400 font-medium"
                         >
                             Fale Conosco <FaWhatsapp className="ml-2 h-8" />
                         </a>
                     </div>
                     <div className="absolute right-0 flex-shrink-0" style={{ top: '50px', zIndex: 10 }}>
-                        <img
+                        {/* <img
                             src={backgroundImage}
                             alt="Imagem vetorial de engenheiro"
                             className="w-full h-auto max-w-lg object-cover mt-[4.8rem] mr-[7rem]"
-                        />
+                        /> */}
                     </div>
                 </section>
             </div>

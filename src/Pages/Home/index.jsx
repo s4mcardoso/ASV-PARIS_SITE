5f41c7d14e97b4f410f362e3efbdf45721cd007f
Samucada <samuelsandro11@gmail.com>
2024-10-16 20:28:38 -0300

    .

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,11 +2,13 @@ import backgroundImage from '../../assets/9557059hd.png';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import { FaWhatsapp } from 'react-icons/fa';
+import backgroundImage2 from '../../assets/seguro.jpg';
 
 function Home() {
     return (
         <>
             <Header />
+            {/* HOME */}
             <div className="relative flex flex-col min-h-screen bg-[var(--cor-amarelo)]">
                 <section className="flex relative container mx-auto w-full h-screen items-center justify-between">
                     <div className="flex flex-col justify-start items-start p-10 max-w-2xl w-full gap-3 backdrop-blur-md rounded-lg shadow-lg bg-white z-20">
@@ -35,7 +37,38 @@ function Home() {
                     </div>
                 </section>
             </div>
-            
+
+            {/* PRÓXIMA SEÇÃO COM BACKGROUND IMAGE E OVERLAY */}
+            <div className="relative flex flex-col min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage2})` }}>
+                {/* Overlay azul opaco */}
+                <div className="absolute inset-0 bg-blue-900 opacity-70"></div>
+
+                <section className="flex relative container mx-auto w-full h-screen items-center justify-between z-10">
+                    <div className="flex flex-col justify-start items-start p-10 max-w-2xl w-full gap-3 backdrop-blur-md rounded-lg shadow-lg bg-white z-20">
+                        <h1 className="text-6xl font-bold text-left text-[#003366]">
+                            Crie com Nossa Equipe
+                        </h1>
+                        <p className="text-2xl mt-2 text-left text-[#333333]">
+                            Nossa equipe dedicada está pronta para transformar seus sonhos em realidade.
+                        </p>
+                        <a
+                            href={`https://wa.me/${5515988150433}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mt-5 inline-flex items-center px-5 py-2 bg-[var(--cor-amarelo)] text-white rounded-lg transition-colors duration-300 hover:bg-yellow-400 font-medium"
+                        >
+                            Fale Conosco <FaWhatsapp className="ml-2 h-8" />
+                        </a>
+                    </div>
+                    <div className="absolute right-0 flex-shrink-0" style={{ top: '50px', zIndex: 10 }}>
+                        <img
+                            src={backgroundImage}
+                            alt="Imagem vetorial de engenheiro"
+                            className="w-full h-auto max-w-lg object-cover mt-[4.8rem] mr-[7rem]"
+                        />
+                    </div>
+                </section>
+            </div>
             <Footer />
         </>
     );

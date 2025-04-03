d03b07300e509544943f19db4ca21af8e302fdb0
Samucada <samuelsandro11@gmail.com>
2025-04-03 16:40:27 -0300

    arrumando bug do mapa no footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,14 +1,18 @@
 import { FaWhatsapp, FaInstagram } from 'react-icons/fa';
-import { memo } from 'react';
+import { memo, useState } from 'react';
 
 const Footer = memo(() => {
+  const [reloadMap, setReloadMap] = useState(false);
+
+  const handleMapError = () => {
+    setReloadMap(!reloadMap); // Força o React a recriar o iframe
+  };
+
   return (
     <footer className="w-full custom-bg-blue py-2 px-4 md:py-4 md:px-8 lg:py-6 lg:px-12">
       <div className="container mx-auto flex flex-col lg:flex-row items-start sm:items-center justify-between">
         {/* Ícones (primeiro no mobile) */}
         <div className="flex gap-2 items-center mb-4 lg:mb-6 order-1 sm:order-1 md:order-1 lg:order-2 -ml-0 sm:ml-0">
-
-
           <a
             href={`https://wa.me/5515988150433`}
             target="_blank"
@@ -35,23 +39,20 @@ const Footer = memo(() => {
             <div className="text-sm text-gray-300">E-mail: asvpariseng@gmail.com</div>
           </div>
         </nav>
-
-        {/* Div vazia (oculta em mobile) */}
-        <div className="hidden lg:flex items-center p-2 order-3">
-          {/* Conteúdo opcional */}
-        </div>
       </div>
       {/* Mapa e copyright */}
       <div className="text-center text-white mt-4">
         <div className="relative flex flex-col p-4">
           <div className="flex justify-center">
             <iframe
+              key={reloadMap} // Força recarga quando há erro
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.153425526579!2d-47.5153313254411!3d-23.49098265900891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c5f4b9881029a7%3A0x6c4b8be28a23c763!2sAv.%20Elias%20Maluf%2C%202067%20-%20Wanel%20Ville%2C%20Sorocaba%20-%20SP%2C%2018055-215!5e0!3m2!1spt-BR!2sbr!4v1729847116727!5m2!1spt-BR!2sbr"
               width="100%"
-              height="100" // Reduzido para mobile
-              style={{ border: 0 }}
+              height="200"
+              style={{ border: 0, minHeight: "200px" }}
               allowFullScreen
               loading="lazy"
+              onError={handleMapError} // Detecta erro e recarrega
             />
           </div>
           <p className="mt-4">&copy; 2024 ASV PARIS | Todos os direitos reservados.</p>
